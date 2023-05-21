5b3ac9690609864ddc19e8eeadf509291e9161f6
miguel <miguelpositivo02@gmail.com>
2023-05-21 19:00:24 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int numero;
+
+Console.WriteLine("digite um numero");
+numero =Convert.ToInt32(Console.ReadLine());
+int definiçao = numero % 2;
+
+if(definiçao == 0){
+    Console.WriteLine($"{numero} e par");
+}
+else{
+    Console.WriteLine($"{numero} e impar");
+}
+
+
